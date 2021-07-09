46f9d127549977427e55f291a1fe7a3d5708c6d1
Pablo Gomez <pablonahuelgomez@gmail.com>
2021-07-09 11:04:31 -0300

    Add endpoint for leagues/{leagueid}/teams

--- routes/spec.js
+++ routes/spec.js
@@ -3899,6 +3899,36 @@ You can find data that can be used to convert hero and ability IDs and other inf
         },
       },
     },
+    '/leagues/{league_id}/teams': {
+      get: {
+        summary: 'GET /leagues/{league_id}/teams',
+        description: 'Get teams for a league',
+        tags: ['leagues'],
+        parameters: [params.leagueIdPathParam],
+        responses: {
+          200: {
+            description: 'Success',
+            schema: teamObject,
+          },
+        },
+        route: () => '/leagues/:league_id/teams',
+        func: (req, res, cb) => {
+          db.raw(`SELECT team_rating.*, teams.*
+            FROM matches
+            LEFT JOIN team_match using(match_id)
+            LEFT JOIN teams using(team_id)
+            LEFT JOIN team_rating using(team_id)
+            WHERE matches.leagueid = ?
+            GROUP BY (teams.team_id, team_rating.team_id)`, [req.params.league_id])
+            .asCallback((err, result) => {
+              if (err) {
+                return cb(err);
+              }
+              return res.json(result.rows);
+            });
+        },
+      },
+    },
     '/teams': {
       get: {
         summary: 'GET /teams',
